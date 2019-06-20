268d1b0fc992aca0250a9e8e402262780c21bcff
Benoit Moussaud <bmoussaud@xebialabs.com>
2019-06-20 07:54:33 +0200

    update tomcat yaml files

--- run-tomcat.sh
+++ run-tomcat.sh
@@ -1,4 +1,5 @@
 mvn clean package
 APP_VERSION=2.0-92
-./xlw apply --xl-deploy-url http://localhost:4516 -f tomcat/xebialabs.yaml --values appversion=$APP_VERSION,title=$APP_VERSION
-
+./xlw apply  -f tomcat/xebialabs.yaml --values appversion=$APP_VERSION,title=$APP_VERSION
+./xlw preview  -f tomcat/deployment.yaml
+./xlw apply  -f tomcat/deployment.yaml
