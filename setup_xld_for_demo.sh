076d0e5715764ff13a18ed2ce0126dc4add71dd3
Benoit Moussaud <bmoussaud@xebialabs.com>
2020-02-28 12:55:25 +0100

    yaml

--- setup_xld_for_demo.sh
+++ setup_xld_for_demo.sh
@@ -1,14 +1,13 @@
 set -x 
-XLD_DEMO_HOME=/tmp/xld-devops-as-code/demo
-DIR_NAME=xl-deploy-9.5.1-server
-mkdir -p ${XLD_DEMO_HOME}
-cd ${XLD_DEMO_HOME}
-if [[ ! -e xld.zip ]]; then
-  curl --output xld.zip https://dist.xebialabs.com/public/trial/xl-deploy/9.5.1/xl-deploy-9.5.1-server-trial-edition.zip
+XLD_DEMO_HOME=./xld-devops-as-code
+DIR_NAME=xl-deploy-9.5.2-server
+if [[ ! -e /tmp/xld.zip ]]; then
+  curl --output /tmp/xld.zip https://dist.xebialabs.com/public/trial/xl-deploy/9.5.2/xl-deploy-9.5.2-server-trial-edition.zip
 fi
-ls -l xld.zip
-unzip -o xld.zip -d ${XLD_DEMO_HOME}
-curl -L --out ${DIR_NAME}/plugins/xld-smoke-test-plugin-1.0.7.xldp https://github.com/xebialabs-community/xld-smoke-test-plugin/releases/download/v1.0.7/xld-smoke-test-plugin-1.0.7.xldp
+ls -l /tmp/xld.zip
+mkdir -p ${XLD_DEMO_HOME}
+unzip -o /tmp/xld.zip -d ${XLD_DEMO_HOME}
+curl -L --out ${XLD_DEMO_HOME}/${DIR_NAME}/plugins/xld-smoke-test-plugin-1.0.7.xldp https://github.com/xebialabs-community/xld-smoke-test-plugin/releases/download/v1.0.7/xld-smoke-test-plugin-1.0.7.xldp
 cp ~/xl-licenses/deployit-license.lic ${XLD_DEMO_HOME}/${DIR_NAME}/conf
 echo "cp -r ./ext/* ${XLD_DEMO_HOME}/${DIR_NAME}/ext"
 echo "${XLD_DEMO_HOME}/${DIR_NAME}/bin/run.sh"
